3337101621249d245c2643f8fbfee00bd8a741c7
XunBach <86772241+XunBach@users.noreply.github.com>
2021-11-07 20:37:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Zinnnnnn à!',
+    introTitle: 'Nhizzz à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là thích bạn <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho Bách <3',
     reply: 'Yêu không có lí do <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hết dịch công chúa đi chơi với anh nhaa , còn không thì qua chăm em vs anh cũng được .',
+    mess: 'Tui biết mà 🥰. Yêu bae nhiều nhiều 😘😘',
+    messDesc: 'Vô Đà Nẵng bae đi chơi với tui nhaa , còn không thì qua chăm em vs tui cũng được .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
